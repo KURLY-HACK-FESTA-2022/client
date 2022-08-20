fb9911678f9a8d67b41341f44df7bf2925874a9d
이재하 <jhl2619@naver.com>
2022-08-21 01:11:27 +0900

    feat: Add Icon file

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,30 @@
+export { ReactComponent as BellIcon } from './BellIcon.svg';
+export { ReactComponent as BellIconAlert } from './BellIconAlert.svg';
+export { ReactComponent as CalendarIcon } from './CalendarIcon.svg';
+export { ReactComponent as CheckCircle } from './CheckCircle.svg';
+export { ReactComponent as CheveronDown } from './CheveronDown.svg';
+export { ReactComponent as CheveronRight } from './CheveronRight.svg';
+export { ReactComponent as Exclamation } from './XIcon.svg';
+export { ReactComponent as LeftAroowIcon } from './LeftAroowIcon.svg';
+export { ReactComponent as MenuIcon } from './MenuIcon.svg';
+export { ReactComponent as XIcon } from './XIcon.svg';
+
+//
+
+export { ReactComponent as DifferenceIcon } from './DifferenceIcon.svg';
+export { ReactComponent as WarningIcon } from './WarningIcon.svg';
+export { ReactComponent as BewareIcon } from './BewareIcon.svg';
+
+// no choice balloon icon
+
+export { ReactComponent as GrayBalloonIcon } from './GrayBalloonIcon.svg';
+export { ReactComponent as RedBalloonIcon } from './RedBalloonIcon.svg';
+export { ReactComponent as YellowBalloonIcon } from './YellowBalloonIcon.svg';
+export { ReactComponent as PurpleBalloonIcon } from './PurpleBalloonIcon.svg';
+
+// choice balloon icon
+
+export { ReactComponent as GrayBalloonChoiceIcon } from './GrayBalloonChoiceIcon.svg';
+export { ReactComponent as RedBalloonChoiceIcon } from './RedBalloonChoiceIcon.svg';
+export { ReactComponent as YellowBalloonChoiceIcon } from './YellowBalloonChoiceIcon.svg';
+export { ReactComponent as PurpleBalloonChoiceIcon } from './PurpleBalloonChoiceIcon.svg';
