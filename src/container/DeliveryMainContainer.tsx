0ecb633aa11f51dd837f40c8c313963061bd855c
이재하 <jhl2619@naver.com>
2022-08-21 18:06:08 +0900

    feat: Add delivery infomation to delivery main container

--- src/container/DeliveryMainContainer.tsx
+++ src/container/DeliveryMainContainer.tsx
@@ -1,9 +1,11 @@
+import DeliveryInfomation from 'components/delivery/main/DeliveryInfomation';
 import KakaoMap from 'components/delivery/main/KakaoMap';
 
 function DeliveryMainContainer() {
   return (
     <>
       <KakaoMap />
+      <DeliveryInfomation />
     </>
   );
 }
