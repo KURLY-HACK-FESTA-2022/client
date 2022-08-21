99adc377dbcd9b30b04b84262bd3147b7d46e5de
이재하 <jhl2619@naver.com>
2022-08-21 13:47:42 +0900

    feat: Add DeliveryMainContainer.tsx for separate logic

---
+++ src/container/DeliveryMainContainer.tsx
@@ -0,0 +1,10 @@
+import KakaoMap from 'components/delivery/main/KakaoMap';
+
+function DeliveryMainContainer() {
+  return (
+    <>
+      <KakaoMap />
+    </>
+  );
+}
+export default DeliveryMainContainer;
