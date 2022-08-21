b1e53449431e17b3bd6dac9c70d3cd2e7b549e96
이재하 <jhl2619@naver.com>
2022-08-21 13:48:54 +0900

    feat: Rename test to delivery

---
+++ src/pages/DeliveryMainPage.tsx
@@ -0,0 +1,12 @@
+import DeliveryMainContainer from 'container/DeliveryMainContainer';
+import React from 'react';
+
+function DeliveryMainPage() {
+  return (
+    <>
+      <DeliveryMainContainer />
+    </>
+  );
+}
+
+export default DeliveryMainPage;
