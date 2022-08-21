ee31a0466b13bc600a65751584c7186a68e80ad8
이재하 <jhl2619@naver.com>
2022-08-21 18:04:31 +0900

    feat: Add delivery infomation to delivery main page

---
+++ src/components/delivery/main/DeliveryInfomation.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function DeliveryInfomation() {
+  return (
+    <section>
+      <h2>상세 정보</h2>
+      <ul>
+        <li>상세주소</li>
+        <li>배송 메시지</li>
+        <li>공동 현관 비밀번호</li>
+        <li>운송장 내역</li>
+        <li>제품 수량</li>
+      </ul>
+      <button>전체보기</button>
+    </section>
+  );
+}
+
+export default DeliveryInfomation;
