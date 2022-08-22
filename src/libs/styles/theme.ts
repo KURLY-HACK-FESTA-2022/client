bd898ccfc308936fd0496f8c4c7c18d4c309d02e
이재하 <jhl2619@naver.com>
2022-08-22 16:14:43 +0900

    fix: Fix typo in theme.ts

--- src/libs/styles/theme.ts
+++ src/libs/styles/theme.ts
@@ -10,8 +10,8 @@ const palette = {
   gray100: '#F1F1F4',
   gray300: '#D7D7D9',
   gray500: '#78787B',
-  gray700: '##454547',
-  gray800: '##202021',
+  gray700: '#454547',
+  gray800: '#202021',
 } as const;
 
 const fontSize = {
