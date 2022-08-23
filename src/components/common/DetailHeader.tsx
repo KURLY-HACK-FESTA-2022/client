80f062b012bcfe7db861a0315a80b87cca166705
이재하 <jhl2619@naver.com>
2022-08-23 15:31:05 +0900

    feat: Add header delivery detail page

---
+++ src/components/common/DetailHeader.tsx
@@ -0,0 +1,27 @@
+import { XIcon } from 'assets/icons';
+import React from 'react';
+import styled from 'styled-components';
+
+function DetailHeader() {
+  return (
+    <HeaderContainer>
+      <XIcon />
+      <HeaderText>상세 정보</HeaderText>
+    </HeaderContainer>
+  );
+}
+
+const HeaderContainer = styled.header`
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 56px;
+  padding: 0 24px;
+`;
+
+const HeaderText = styled.h2`
+  margin-left: 22px;
+  ${({ theme }) => theme.textStyle.bold[2028]}
+`;
+
+export default DetailHeader;
