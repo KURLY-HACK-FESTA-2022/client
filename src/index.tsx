30d2c6aff4474ab04d8b256e878483f375683f9a
이재하 <jhl2619@naver.com>
2022-08-23 19:15:18 +0900

    feat: Move react query to App.ts

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
+
 import { BrowserRouter } from 'react-router-dom';
 import { HelmetProvider } from 'react-helmet-async';
 import './libs/styles/global.css';
@@ -11,17 +10,12 @@ import './libs/styles/global.css';
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
-const queryClient = new QueryClient();
 root.render(
-  <QueryClientProvider client={queryClient}>
-    {/* devtools */}
-    <ReactQueryDevtools initialIsOpen={true} />
-    <HelmetProvider>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </HelmetProvider>
-  </QueryClientProvider>,
+  <HelmetProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </HelmetProvider>,
 );
 
 serviceWorkerRegistration.unregister();
