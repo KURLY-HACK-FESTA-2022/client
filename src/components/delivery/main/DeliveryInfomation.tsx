4f25c528ae8a59d1f96b9cfa36c7d35b3ad5fcaa
이재하 <jhl2619@naver.com>
2022-08-24 06:04:13 +0900

    feat: Show delivery that click on kakao map

--- src/components/delivery/main/DeliveryInfomation.tsx
+++ src/components/delivery/main/DeliveryInfomation.tsx
@@ -1,41 +1,77 @@
 import { XIcon } from 'assets/icons';
+import Button from 'components/common/Button';
+import { deliveryList } from 'libs/utils/mock';
+import { Delivery } from 'model/delivery';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Path from 'routes/Path';
 import styled from 'styled-components';
 
-function DeliveryInfomation() {
+interface Props {
+  selectedOrder: number;
+}
+
+function DeliveryInfomation({ selectedOrder }: Props) {
+  const navigate = useNavigate();
   return (
     <InfomationSection>
       <TitleContainer>
         <Title>상세 정보</Title>
         <XIcon />
       </TitleContainer>
-      <CategoryList>
-        <Category>
-          상세주소
-          <CategoryContent>서울시 강남구 역삼동</CategoryContent>
-        </Category>
-        <Category>배송 메시지</Category>
-        <CategoryContent>안녕하세요. 저희 판매자입니다.</CategoryContent>
-        <Category>
-          공동 현관 비밀번호
-          <CategoryContent>123456</CategoryContent>
-        </Category>
-        <Category>
-          운송장 내역
-          <CategoryContent>123456789</CategoryContent>
-        </Category>
-        <Category>
-          제품 수량
-          <CategoryContent>1</CategoryContent>
-        </Category>
-      </CategoryList>
+      {deliveryList.map(
+        (delivery: Delivery) =>
+          delivery.id === selectedOrder && (
+            <CategoryList>
+              <Category>
+                상세주소
+                <CategoryContent>{delivery.address}</CategoryContent>
+              </Category>
+              <Category>
+                배송 메시지
+                <CategoryContent>{delivery.comment}</CategoryContent>
+              </Category>
+              <Category>
+                공동 현관 비밀번호
+                <CategoryContent>{delivery.entrancePassword}</CategoryContent>
+              </Category>
+              <Category>
+                운송장 내역
+                <CategoryContent>{delivery.orderNumber}</CategoryContent>
+              </Category>
+              <Category>
+                제품 수량
+                <CategoryContent>{delivery.total}</CategoryContent>
+              </Category>
+            </CategoryList>
+          ),
+      )}
+
+      <FullViewButton
+        variant="longWhite"
+        onClick={() => navigate(Path.DELIVERY_DETAIL)}
+      >
+        전체보기
+      </FullViewButton>
     </InfomationSection>
   );
 }
 
+const InfomationSection = styled.section`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  border-radius: 16px 16px 0px 0px;
+  position: relative;
+  bottom: 58px;
+  background: white;
+  z-index: 99;
+  padding: 24px 20px;
+  width: 360px;
+`;
+
 const Title = styled.h2`
   ${({ theme }) => theme.textStyle.bold[2028]};
-  font-size: 28px;
 `;
 
 const CategoryList = styled.ul``;
@@ -58,17 +94,8 @@ const TitleContainer = styled.div`
   align-items: center;
 `;
 
-const InfomationSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  border-radius: 16px 16px 0px 0px;
-  position: relative;
-  bottom: 32px;
-  background: white;
-  z-index: 99;
-  padding: 24px 20px;
-  width: 360px;
+const FullViewButton = styled(Button)`
+  margin-top: 16px;
 `;
 
 export default DeliveryInfomation;
--- src/container/DeliveryMainContainer.tsx
+++ src/container/DeliveryMainContainer.tsx
@@ -1,13 +1,38 @@
-import Button from 'components/common/Button';
+import { useQuery } from '@tanstack/react-query';
+import client from 'apis/client';
 import DeliveryInfomation from 'components/delivery/main/DeliveryInfomation';
 import KakaoMap from 'components/delivery/main/KakaoMap';
+import { deliveryList } from 'libs/utils/mock';
+import { queryKeys } from 'libs/utils/queryKeys';
+import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
 function DeliveryMainContainer() {
+  const { id } = useParams();
+  const [selectedOrder, setSelectedOrder] = useState(-1);
+
+  // const getOrderList = async (id: any) => {
+  //   const response = await client.get(`/orders/${id}`);
+  //   return response.data;
+  // };
+
+  // const { data: deliveryList } = useQuery(
+  //   [queryKeys.deliveryList],
+  //   () => getOrderList(id),
+  //   {
+  //     cacheTime: 5 * 1000 * 60,
+  //     staleTime: 5 * 1000 * 60,
+  //   },
+  // );
+
+  // console.log(deliveryList);
   return (
     <>
-      <KakaoMap />
-      <DeliveryInfomation />
-      <Button variant="longWhite">전체보기</Button>
+      <KakaoMap
+        deliveryList={deliveryList}
+        setSelectedOrder={setSelectedOrder}
+      />
+      <DeliveryInfomation selectedOrder={selectedOrder} />
     </>
   );
 }
