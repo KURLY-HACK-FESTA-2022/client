453cf14b6d2dbdadfe99f34977f62cc81d4f83e6
이재하 <jhl2619@naver.com>
2022-08-24 05:55:43 +0900

    feat: Add onClick event to warning modal

--- src/components/delivery/deliveryDetail/WarningModal.tsx
+++ src/components/delivery/deliveryDetail/WarningModal.tsx
@@ -7,15 +7,17 @@ import {
 import Button from 'components/common/Button';
 import ModalTemplate from 'components/common/ModalTemplate';
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import Path from 'routes/Path';
 import styled from 'styled-components';
 
 interface Props {
+  deliveryResponse: any;
   onToggleModal: () => void;
 }
 
-function WarningModal({ onToggleModal }: Props) {
-  const deliveryComplateBranchResponse = 'difference';
-
+function WarningModal({ deliveryResponse, onToggleModal }: Props) {
+  const navigate = useNavigate();
   const deliveryComplateBranch = [
     {
       branch: 'difference',
@@ -23,10 +25,20 @@ function WarningModal({ onToggleModal }: Props) {
       icon: <HighWarningIcon />,
       button: (
         <>
-          <Button variant="white" width={132} height={44}>
+          <Button
+            variant="white"
+            width={132}
+            height={44}
+            onClick={() => navigate(Path.DELIVERY_COMPLETE)}
+          >
             예
           </Button>
-          <Button variant="purple" width={132} height={44}>
+          <Button
+            variant="purple"
+            width={132}
+            height={44}
+            onClick={onToggleModal}
+          >
             아니오
           </Button>
         </>
@@ -36,28 +48,51 @@ function WarningModal({ onToggleModal }: Props) {
       branch: 'complaint',
       text: '문의 및 민원이 잦은 고객입니다. \n배송에 유의 부탁드립니다.',
       icon: <MidWarningIcon />,
-      button: <Button variant="longPurple">예</Button>,
+      button: (
+        <Button
+          variant="longPurple"
+          onClick={() => navigate(Path.DELIVERY_COMPLETE)}
+        >
+          예
+        </Button>
+      ),
     },
     {
       branch: 'undelivery',
       text: '미배송으로 인한 재배송 신청 고객입니다. \n주소를 한번 더 점검해주세요',
       icon: <LowWarningIcon />,
-      button: <Button variant="longPurple">예</Button>,
+      button: (
+        <Button
+          variant="longPurple"
+          onClick={() => navigate(Path.DELIVERY_COMPLETE)}
+        >
+          예
+        </Button>
+      ),
     },
     {
       branch: 'incorrectDelivery',
       text: '오배송 실수가 잦은 구역입니다. \n유의하여 배송 부탁드립니다.',
       icon: <LowWarningIcon />,
-      button: <Button variant="longPurple">예</Button>,
+      button: (
+        <Button
+          variant="longPurple"
+          onClick={() => navigate(Path.DELIVERY_COMPLETE)}
+        >
+          예
+        </Button>
+      ),
     },
   ];
   return (
     <ModalTemplate width={320} height={308} onToggleModal={onToggleModal}>
       <ModalInnerStyled>
-        <XIconStyled />
+        <XIconWrapper>
+          <XIcon />
+        </XIconWrapper>
         {deliveryComplateBranch.map(
           ({ branch, text, icon, button }) =>
-            deliveryComplateBranchResponse === branch && (
+            deliveryResponse.branch === branch && (
               <div key={branch}>
                 <WarningIcon>{icon}</WarningIcon>
                 <WarningText>{text}</WarningText>
@@ -70,20 +105,18 @@ function WarningModal({ onToggleModal }: Props) {
   );
 }
 
-const ModalInnerStyled = styled.div`
-  padding: 10px;
-`;
+const ModalInnerStyled = styled.div``;
 
-const XIconStyled = styled(XIcon)`
-  position: relative;
-  top: 10px;
-  left: 273px;
+const XIconWrapper = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  margin: 30px 30px 0 0;
 `;
 
 const WarningIcon = styled.div`
   display: flex;
   justify-content: center;
-  margin-top: 72px;
+  margin-top: 30px;
 `;
 
 const WarningText = styled.span`
